a80f7351b0c11c01e1f4951417c8c5533aad5c8d
fajarrosi <fajarilhamrosi@gmail.com>
2023-01-26 17:28:18 +0700

    init order create

--- service-order-payment/app/Http/Controllers/OrderController.php
+++ service-order-payment/app/Http/Controllers/OrderController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Order;
 
 class OrderController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        # code...
+    }
+    private function getMidtransSnapurl($params){
+
+        // Set your Merchant Server Key
+        \Midtrans\Config::$serverKey = env('MIDTRANS_SERVER_KEY');
+        // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+        \Midtrans\Config::$isProduction = (bool) env('MIDTRANS_PRODUCTION');
+        // Set 3DS transaction for credit card to true
+        \Midtrans\Config::$is3ds = (bool) env('MIDTRANS_3DS');
+        
+        $snapUrl = \Midtrans\Snap::createTransaction($params)->redirect_url;
+        return $snapUrl;
+    }
 }
